7c094d698cc940ea7bbc76418a8e23795be603e0
soysnorre <soysmorre@gmail.com>
2022-06-21 14:00:13 +0200

    Correción errores

--- Firmware/Marlin-bugfix-2.0.9.2.x/Marlin/Configuration_adv.h
+++ Firmware/Marlin-bugfix-2.0.9.2.x/Marlin/Configuration_adv.h
@@ -1386,12 +1386,12 @@
    */
   #define POWER_LOSS_RECOVERY
   #if ENABLED(POWER_LOSS_RECOVERY)
-    #define PLR_ENABLED_DEFAULT   true // Power Loss Recovery enabled by default. (Set with 'M413 Sn' & M500)
-    #define BACKUP_POWER_SUPPLY       // Backup power / UPS to move the steppers on power loss
-    #define POWER_LOSS_ZRAISE       2 // (mm) Z axis raise on resume (on power loss with UPS)
-    #define POWER_LOSS_PIN       PS_ON_PIN // Pin to detect power loss. Set to -1 to disable default pin on boards without module.
-    #define POWER_LOSS_STATE     HIGH // State of pin indicating power loss
-    #define POWER_LOSS_PULLUP         // 3DWORK PLR habilitamos PULLUP SKR // Set pullup / pulldown as appropriate for your sensor
+    #define PLR_ENABLED_DEFAULT   true     // Power Loss Recovery enabled by default. (Set with 'M413 Sn' & M500)
+    #define BACKUP_POWER_SUPPLY            // Backup power / UPS to move the steppers on power loss
+    #define POWER_LOSS_ZRAISE     2        // (mm) Z axis raise on resume (on power loss with UPS)
+    #define POWER_LOSS_PIN                 // Pin to detect power loss. Set to -1 to disable default pin on boards without module.
+    #define POWER_LOSS_STATE     HIGH      // State of pin indicating power loss
+    #define POWER_LOSS_PULLUP              // 3DWORK PLR habilitamos PULLUP SKR // Set pullup / pulldown as appropriate for your sensor
     //#define POWER_LOSS_PULLDOWN
     #define POWER_LOSS_PURGE_LEN   20 // (mm) Length of filament to purge on resume
     //#define POWER_LOSS_RETRACT_LEN 10 // (mm) Length of filament to retract on fail. Requires backup power.
@@ -2401,7 +2401,7 @@
  *
  * Enable PARK_HEAD_ON_PAUSE to add the G-code M125 Pause and Park.
  */
-//#define ADVANCED_PAUSE_FEATURE
+#define ADVANCED_PAUSE_FEATURE                    //3dWORK MEJORA
 #if ENABLED(ADVANCED_PAUSE_FEATURE)
   #define PAUSE_PARK_RETRACT_FEEDRATE         60  // (mm/s) Initial retract feedrate.
   #define PAUSE_PARK_RETRACT_LENGTH            2  // (mm) Initial retract.
